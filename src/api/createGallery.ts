28b944d5c1e521a3fd683d10e6bc9e9d1a8fb242
Filipos <83700574+pidizman@users.noreply.github.com>
2022-10-10 11:10:00 +0000

    Pár novinek

--- src/api/createGallery.ts
+++ src/api/createGallery.ts
@@ -5,15 +5,16 @@ import db from "quick.db";
 export const CreateGallery = (req: Request, res: Response) => {
   try {
     validateGalleryData(req.body.galleryProperties);
-  } catch(e) {
+  }catch(e) {
     return console.log(`Missing ${e}`); 
   };
 
   //db.set("Filipos_galleries",[])
   const galleries = db.get(`${req.body.name}_galleries`);
-const y = req.body.galleryProperties;
+  const y = req.body.galleryProperties;
 
-const x = galleries.push(y);
-db.set(`${req.body.name}_galleries`, galleries);
-console.log(db.get(`${req.body.name}_galleries`));
+  const x = galleries.push(y);
+  //db.delete(`${req.body.name}_galleries`);
+  db.set(`${req.body.name}_galleries`, galleries);
+  console.log(db.get(`${req.body.name}_galleries`));
 };
